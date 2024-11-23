e17e15bcee60318e0dbd047a42bfdec85fda2072
tamu0x8 <ahmetpkavas@gmail.com>
2024-11-23 15:40:42 +0300

    Working Report

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 def get_noc_from_dict(dict):
-    return 1
+    for char in dict:
+        return dict[char]
 
 def create_report_from_list(ls):
-    return 1
+    ls.sort(reverse = True, key=get_noc_from_dict)
+    sorted_list = ls
+    for dicts in sorted_list:
+        for char in dicts:
+            print(f"The {char} was found {dicts[char]} times in")
+
 
 
 def create_list_of_dicts(dict):
@@ -35,8 +41,9 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         word_count = count_words(file_contents)
-        print(word_count)
-        print(unique_char_count(file_contents))
+        uniq_char_dict = unique_char_count(file_contents)
+        list_of_dicts = create_list_of_dicts(uniq_char_dict) 
+        create_report_from_list(list_of_dicts)
 
 
 
